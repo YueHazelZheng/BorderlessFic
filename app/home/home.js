93f8787743b6f21f39909ff5fd7414f7c264fdad
YueHazelZheng <yue@berkeley.edu>
2016-01-09 15:45:03 -0500

    connect article page with other pages

--- app/home/home.js
+++ app/home/home.js
@@ -10,46 +10,46 @@ angular.module('myApp.home', ['ngRoute'])
 }])
 
 .filter('filterMultiple',['$filter',function ($filter) {
-return function (items, keyObj) {
-    var filterObj = {
-        data:items,
-        filteredData:[],
-        applyFilter : function(obj,key){
-            var fData = [];
-            if (this.filteredData.length == 0)
-                this.filteredData = this.data;
-            if (obj){
-                var fObj = {};
-                if (!angular.isArray(obj)){
-                    fObj[key] = obj;
-                    fData = fData.concat($filter('filter')(this.filteredData,fObj));
-                } else if (angular.isArray(obj)){
-                    if (obj.length > 0){
-                        for (var i=0;i<obj.length;i++){
-                            if (angular.isDefined(obj[i])){
-                                fObj[key] = obj[i];
-                                fData = fData.concat($filter('filter')(this.filteredData,fObj));    
+    return function (items, keyObj) {
+        var filterObj = {
+            data:items,
+            filteredData:[],
+            applyFilter : function(obj,key){
+                var fData = [];
+                if (this.filteredData.length == 0)
+                    this.filteredData = this.data;
+                if (obj){
+                    var fObj = {};
+                    if (!angular.isArray(obj)){
+                        fObj[key] = obj;
+                        fData = fData.concat($filter('filter')(this.filteredData,fObj));
+                    } else if (angular.isArray(obj)){
+                        if (obj.length > 0){
+                            for (var i=0;i<obj.length;i++){
+                                if (angular.isDefined(obj[i])){
+                                    fObj[key] = obj[i];
+                                    fData = fData.concat($filter('filter')(this.filteredData,fObj));    
+                                }
                             }
-                        }
 
+                        }
+                    }
+                    if (fData.length > 0){
+                        this.filteredData = fData;
                     }
                 }
-                if (fData.length > 0){
-                    this.filteredData = fData;
-                }
             }
+        };
+        if (keyObj){
+            angular.forEach(keyObj,function(obj,key){
+                filterObj.applyFilter(obj,key);
+            });
         }
+        return filterObj.filteredData;
     };
-    if (keyObj){
-        angular.forEach(keyObj,function(obj,key){
-            filterObj.applyFilter(obj,key);
-        });
-    }
-    return filterObj.filteredData;
-}
 }])
  
-.controller('HomeCtrl', ['$scope', 'CommonProp', '$firebase', function($scope,CommonProp, $firebase) {
+.controller('HomeCtrl', ['$scope', 'CommonProp', '$firebase', '$location', function($scope,CommonProp,$firebase,$location) {
  	var firebaseObj = new Firebase("https://resplendent-heat-9609.firebaseio.com");
     $scope.userid = CommonProp.getUser();
  	$scope.langPref = CommonProp.getLangPref();
@@ -106,6 +106,11 @@ return function (items, keyObj) {
 
     $scope.filter = function() {
 
+    };
+
+    $scope.readMore = function(id) {
+        CommonProp.setArticle(id);
+        $location.path('/article');
     };
 
      // if no preference is chosen
